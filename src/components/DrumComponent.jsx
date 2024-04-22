9ceee685053dc924b995719d795d545dd88dcd8f
JPBarrientos <jpbarrientosros@outlook.com>
2024-04-22 00:29:25 -0300

    Animaciones al presionar botones

--- src/components/DrumComponent.jsx
+++ src/components/DrumComponent.jsx
@@ -1,81 +1,98 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
+import { kickAudio, snareAudio, hihatAudio, crash1Audio, tom1Audio, tom2Audio, crash2Audio, rideAudio } from '/src/utils/audioFiles';
 
-const DrumComponent = ({ diametro, tipo, keyName, onClick }) => {
-    let componentClass, imgClass, imgSrc, imgAlt;
-  
-    switch (tipo) {
-      case 'platillo':
-        componentClass = 'drum-cymbal';
-        imgClass = 'logo bottom';
-        imgSrc = '/src/assets/images/cymbal-logo.png';
-        imgAlt = 'Logo de platillo';
-        break;
-      case 'cuerpo':
-        componentClass = 'drum-body';
-        imgClass = 'logo top';
-        imgSrc = '/src/assets/images/drum-logo.png';
-        imgAlt = 'Logo de batería';
-        break;
-    }
-  
-    return (
-      <div className={componentClass} style={{ width: `${diametro}`, height: `${diametro}` }} onClick={onClick}>
-        <img className={imgClass} src={imgSrc} alt={imgAlt} />
-        <p className='key'>{keyName}</p>
-      </div>
-    );
+
+const DrumComponent = ({ diametro, tipo, keyName, onClick, keysPressed }) => {
+  let componentClass, imgClass, imgSrc, imgAlt;
+
+  switch (tipo) {
+    case 'platillo':
+      componentClass = keysPressed.has(keyName.toLowerCase()) ? 'drum-cymbal active' : 'drum-cymbal';
+      imgClass = 'logo bottom';
+      imgSrc = '/src/assets/images/cymbal-logo.png';
+      imgAlt = 'Logo de platillo';
+      break;
+    case 'cuerpo':
+      componentClass = keysPressed.has(keyName.toLowerCase()) ? 'drum-body active' : 'drum-body';
+      imgClass = 'logo top';
+      imgSrc = '/src/assets/images/drum-logo.png';
+      imgAlt = 'Logo de batería';
+      break;
   }
+
+  return (
+    <div className={componentClass} style={{ width: `${diametro}`, height: `${diametro}` }} onClick={onClick}>
+      <img className={imgClass} src={imgSrc} alt={imgAlt} />
+      <p className='key'>{keyName}</p>
+    </div>
+  );
+}
+
+export const DrumKit = () => {
+  const [keysPressed, setKeysPressed] = useState(new Set());
   
-  export const DrumKit = () => {
-    const keySoundMapRef = useRef({
-      ' ': new Audio('/src/assets/audio/kick.wav'),
-      's': new Audio('/src/assets/audio/snare.wav'),
-      'a': new Audio('/src/assets/audio/hihat.wav'),
-      'q': new Audio('/src/assets/audio/crash1.wav'),
-      'd': new Audio('/src/assets/audio/tom1.wav'),
-      'f': new Audio('/src/assets/audio/tom2.wav'),
-      't': new Audio('/src/assets/audio/crash2.wav'),
-      'b': new Audio('/src/assets/audio/ride.wav'),
-    });
-  
-    const playSound = (key) => {
-      const soundFile = keySoundMapRef.current[key];
-      if (soundFile) {
-        soundFile.currentTime = 0; // Reset the audio file to the beginning
-        soundFile.play();
-      }
+  const keySoundMapRef = useRef({
+    'space': kickAudio,
+    's': snareAudio,
+    'a': hihatAudio,
+    'q': crash1Audio,
+    'd': tom1Audio,
+    'f': tom2Audio,
+    't': crash2Audio,
+    'b': rideAudio,
+  });
+
+  const handleKeyDown = (event) => {
+    const key = event.key.trim() || 'space';
+    if (!keysPressed.has(key)) {
+      playSound(key);
+      setKeysPressed(prevKeys => new Set(prevKeys).add(key));
     }
-  
-    useEffect(() => {
-      const handleKeyPress = (event) => {
-        playSound(event.key);
-      };
-  
-      // Add event listener for key press
-      document.addEventListener('keydown', handleKeyPress);
-  
-      // Clean up event listener
-      return () => {
-        document.removeEventListener('keydown', handleKeyPress);
-      };
-    }, []);
-  
-    return (
-      (
-        <>
-          <div className='drum-upper'>
-            <DrumComponent diametro='100px' tipo='platillo' keyName='Q' onClick={() => playSound('q')}></DrumComponent>
-            <DrumComponent diametro='50px' tipo='cuerpo' keyName='D' onClick={() => playSound('d')}></DrumComponent>
-            <DrumComponent diametro='70px' tipo='cuerpo' keyName='F' onClick={() => playSound('f')}></DrumComponent>
-            <DrumComponent diametro='120px' tipo='platillo' keyName='T' onClick={() => playSound('t')}></DrumComponent>
-          </div>
-          <div className='drum-lower'>
-            <DrumComponent diametro='60px' tipo='platillo' keyName='A' onClick={() => playSound('a')}></DrumComponent>
-            <DrumComponent diametro='80px' tipo='cuerpo' keyName='S' onClick={() => playSound('s')}></DrumComponent>
-            <DrumComponent diametro='130px' tipo='cuerpo' keyName='Space' onClick={() => playSound(' ')}></DrumComponent>
-            <DrumComponent diametro='140px' tipo='platillo' keyName='B' onClick={() => playSound('b')}></DrumComponent>
-          </div>
-        </>
-      )
-    );
   };
+
+  const handleKeyUp = (event) => {
+    const key = event.key.trim() || 'space';
+    setKeysPressed(prevKeys => {
+      const updatedKeys = new Set(prevKeys);
+      updatedKeys.delete(key);
+      return updatedKeys;
+    });
+  };
+
+  const playSound = (key) => {
+    const soundFile = keySoundMapRef.current[key];
+    if (soundFile) {
+      soundFile.currentTime = 0;
+      soundFile.play();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
+
+  return (
+    (
+      <>
+        <div className='drum-upper'>
+          <DrumComponent diametro='100px' tipo='platillo' keyName='Q' onClick={() => playSound('q')} keysPressed={keysPressed}></DrumComponent>
+          <DrumComponent diametro='50px' tipo='cuerpo' keyName='D' onClick={() => playSound('d')} keysPressed={keysPressed}></DrumComponent>
+          <DrumComponent diametro='70px' tipo='cuerpo' keyName='F' onClick={() => playSound('f')} keysPressed={keysPressed}></DrumComponent>
+          <DrumComponent diametro='120px' tipo='platillo' keyName='T' onClick={() => playSound('t')} keysPressed={keysPressed}></DrumComponent>
+        </div>
+        <div className='drum-lower'>
+          <DrumComponent diametro='60px' tipo='platillo' keyName='A' onClick={() => playSound('a')} keysPressed={keysPressed}></DrumComponent>
+          <DrumComponent diametro='80px' tipo='cuerpo' keyName='S' onClick={() => playSound('s')} keysPressed={keysPressed}></DrumComponent>
+          <DrumComponent diametro='130px' tipo='cuerpo' keyName='Space' onClick={() => playSound('space')} keysPressed={keysPressed}></DrumComponent>
+          <DrumComponent diametro='140px' tipo='platillo' keyName='B' onClick={() => playSound('b')} keysPressed={keysPressed}></DrumComponent>
+        </div>
+      </>
+    )
+  );
+};
---
+++ src/utils/audioFiles.js
@@ -0,0 +1,8 @@
+export const kickAudio = new Audio('/src/assets/audio/kick.wav');
+export const snareAudio = new Audio('/src/assets/audio/snare.wav');
+export const hihatAudio = new Audio('/src/assets/audio/hihat.wav');
+export const crash1Audio = new Audio('/src/assets/audio/crash1.wav');
+export const tom1Audio = new Audio('/src/assets/audio/tom1.wav');
+export const tom2Audio = new Audio('/src/assets/audio/tom2.wav');
+export const crash2Audio = new Audio('/src/assets/audio/crash2.wav');
+export const rideAudio = new Audio('/src/assets/audio/ride.wav');
